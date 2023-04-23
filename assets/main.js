12b3ccdbd240a4356107634996df551944669108
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-04-23 12:15:20 +0200

    Struttura completata

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+const {createApp} = Vue 
+createApp({
+    data(){
+        return{
+        arrayMail: []
+        }
+    },created(){
+        this.attivaApi()
+    }, methods:{
+        attivaApi(){
+            for (let i = 0; i < 10; i++) {
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                .then(res => {
+                 this.arrayMail.push(res.data.response)
+                 console.log(`arrayMail`)
+            })           
+                
+            }
+        }
+       
+    }
+}).mount(`#app`)
+
